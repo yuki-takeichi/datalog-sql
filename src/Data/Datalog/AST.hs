86536ef853a2e52fc31c2c9aa473021cf89ce8ac
Yuki Takeichi <yuki.takeichi@gmail.com>
2015-10-08 02:22:36 +0900

    Refactor

--- src/Data/Datalog/AST.hs
+++ src/Data/Datalog/AST.hs
@@ -11,7 +11,6 @@ module Data.Datalog.AST (
 , DatalogBody(..)
 , CTE(..)
 , Relation(..)
-, genSQLAST
 --, genSQLString
 --, genSQLStringIndented
 , IndentedString(..)
@@ -20,9 +19,6 @@ module Data.Datalog.AST (
 ) where
 
 import Prelude hiding (lookup, head)
-import Data.List ({-groupBy, -}nub)
-import Data.Maybe (catMaybes, isNothing)
-import Data.Map (Map, lookup)
 --import Data.List.NonEmpty ( NonEmpty(..) )
 
 data DatalogStmt = DatalogFact DatalogBody
@@ -84,88 +80,6 @@ indent is = unlines $ padding 0 is
     shift level = map ((take level $ cycle " ") ++)
 
 
-genSQLAST :: Map String DatalogStmt -- TODO MapのvalueがDatalotStmtだとlookupで毎回分岐の実装を強いられるのが辛いので変更する。
-             -> DatalogStmt
-             -> SelectStmt
-genSQLAST rules stmt = flattenWithClauses $ foldl1 Union $ [ genSimpleSelectAST rules stmt ]
-  where
-    flattenWithClauses :: SelectStmt -> SelectStmt
-    flattenWithClauses = id -- TODO これを入れると読みやすいっていう人と読みにくいっていう人の2パターンいそう。optionalにできるとよい。
-
-
-genSimpleSelectAST :: Map String DatalogStmt -> DatalogStmt -> SelectStmt
-genSimpleSelectAST _ (DatalogFact _) = undefined
-genSimpleSelectAST _ (DatalogQuery _ _) = undefined
-genSimpleSelectAST _ (DatalogRule []) = undefined
-genSimpleSelectAST rules (DatalogRule ((DatalogHead head, DatalogBody body):[])) = SelectStmt {
-  withClauses = _withClauses,
-  selectExprs = _selectExprs,
-  fromTables = _fromTables,
-  whereClause = _whereClause
-}
-  where 
-    varNames :: [String]
-    varNames = nub $ catMaybes $ map varName $ head++body
-
-    --freeVarNames :: [String]
-    --freeVarNames = nub $ catMaybes $ map varName $ head
-
-    --freeVars :: [ TupleAttrRef ]
-    --freeVars = filter isVarRef head
-
-    varName :: TupleAttrRef -> Maybe String
-    varName TupleAttrRef { arg=Var vname } = Just vname
-    varName _ = Nothing
-
-    varAttrs :: String -> [TupleAttrRef] -> [TupleAttrRef]
-    varAttrs vname = filter (\x -> case varName x of
-                                     Just y -> y == vname
-                                     Nothing -> False)
-
-    joins :: [[ TupleAttrRef ]]
-    joins = map (\vname -> varAttrs vname body) varNames -- TODO groupBy を使ってもっと綺麗に書きたいよー
-    -- TODO ?が付いているfactは除外しないと...
-
-    constraints :: [TupleAttrRef]
-    constraints = filter (isNothing.varName) body
-
-    _selectExprs :: [ SelectExpr ]
-    _selectExprs = catMaybes $ map ((fmap (\(h, r) -> SelectExpr (ColumnRef (tableName $ rel r) (attr r)) (Just $ attr h))).hage) head
-      where
-        hage r@TupleAttrRef{arg=Var vname} = Just (r, (varAttrs vname body)!!0) -- !!0は重複ロジックなのでリファクタ必要
-        hage _ = Nothing
-
-    _fromTables :: [ TableRef ]
-    _fromTables = map (\x -> (Table x Nothing)) $ nub $ map (tableName . rel) body -- TODO parent1 parent2問題を解決する
-
-    _withClauses :: [ CTE ]
-    _withClauses = map (uncurry CTE) $ catMaybes $ map findRules $ catMaybes $ map (\x -> case x of
-                                                                        Table name_ _ -> Just name_ -- TODO handle Table alias
-                                                                        _ -> Nothing) _fromTables
-      where
-        findRules :: String -> Maybe (String, SelectStmt)
-        findRules rname = (fmap (\x -> (rname, hogehoge x))) . (\x -> lookup x rules) $ rname
-          where
-            hogehoge :: DatalogStmt -> SelectStmt
-            hogehoge = undefined
-
-
-    _whereClause :: [ Predicate ]
-    _whereClause = catMaybes $ map joinEquals (concat $ map something joins) ++ map constraintEquals constraints
-      where
-        joinEquals :: (TupleAttrRef, TupleAttrRef) -> Maybe Predicate
-        joinEquals (r1@TupleAttrRef{arg=Var _}, r2@TupleAttrRef{arg=Var _}) = Just $ Equal (ColumnRef (tableName $ rel r1) (attr r1))
-                                                                                     (ColumnRef (tableName $ rel r2) (attr r2))
-        joinEquals _ = Nothing
-
-        constraintEquals :: TupleAttrRef -> Maybe Predicate
-        constraintEquals r@TupleAttrRef{arg=Atom aname} = Just $ Equal (ColumnRef (tableName $ rel r) (attr r))
-                                                          (SqlStr (aname))
-        constraintEquals _ = Nothing
-genSimpleSelectAST _ (DatalogRule _) = undefined
-
-tableName :: Relation -> String
-tableName r = (name r) ++ (show $ rid r)
 
 --joins :: [TupleAttrRef] -> [[TupleAttrRef]]
 --joins refs = let varRefs = filter isVarRef refs
@@ -174,13 +88,6 @@ tableName r = (name r) ++ (show $ rid r)
 --isVarRef :: TupleAttrRef -> Bool
 --isVarRef TupleAttrRef{arg=Var _} = True
 --isVarRef _ = False
-
-something :: [a] -> [(a,a)] -- TODO rename
-something [] = []
-something (_:[]) = [] -- 変数への参照がひとつしかない場合は空
-something (x:xs) = reverse $ somethingHoge x xs []
-  where somethingHoge _ [] piyo = piyo
-        somethingHoge y (y':ys) piyo = somethingHoge y ys $ (y, y') : piyo
 
 {-
 genSQLString :: SelectStmt -> IndentedString
--- test/Test.hs
+++ test/Test.hs
@@ -8,6 +8,7 @@ import Test.HUnit (Assertion, assertBool, (@=?))
 --import Test.QuickCheck
 
 import ParserTest
+import SQLTranslatorTest
 
 import Data.Map as M
 import Data.Datalog.AST
@@ -22,13 +23,10 @@ pass = assertBool "pass" True
 
 tests :: [Test]
 tests = [
-          parserTests,
           testGroup "Integration: Transtale datalog code to SQL code" [
           ],
-          testGroup "Translate datalog AST to SQL AST" [
-            testCase "query with conjunctive rule" test_genSQLASTWithNoRecursiveWithClause,
-            testCase "query with recursive and conjunctive rule" undefined
-          ],
+          parserTests,
+          sqlTranslatorTests,
           {-testGroup "query" [
             testCase "always success" pass,
             testCase "genSQL" test_genSQLAST1,
@@ -41,102 +39,6 @@ tests = [
         ]
 
 
--- ?(him: X) :- ancestor(me: yuki, him: X), grandparent(me: masaki, him: X).
-{- select ancestor.him as him
- - from ancestor
- -    , grandparent
- - where ancestor.him = grandparent.him
- -   and ancestor.me = "yuki"
- -   and grandparent.me = "masaki"
- - ;
- -}
-test_genSQLAST1 :: Assertion
-test_genSQLAST1 = sql @=? genSQLAST M.empty (DatalogQuery queryHead queryBody)
-  where
-    queryHead :: DatalogHead
-    queryHead = DatalogHead [ 
-                  TupleAttrRef {rel=Relation{name="?", rid=0}, attr="him", arg=Var "X"}
-                ]
-    queryBody :: DatalogBody
-    queryBody = DatalogBody [
-                  TupleAttrRef {rel=Relation{name="ancestor", rid=0}, attr="me", arg=Atom "yuki"},
-                  TupleAttrRef {rel=Relation{name="ancestor", rid=0}, attr="him", arg=Var "X"},
-                  TupleAttrRef {rel=Relation{name="grandparent", rid=0}, attr="me", arg=Atom "masaki"},
-                  TupleAttrRef {rel=Relation{name="grandparent", rid=0}, attr="him", arg=Var "X"}
-                ]
-    sql :: SelectStmt
-    sql = SelectStmt {
-            withClauses = [],
-            selectExprs = [ SelectExpr (ColumnRef "ancestor" "him") (Just "him") ],
-            fromTables = [ Table "ancestor" Nothing, Table "grandparent" Nothing ],
-            whereClause = [ Equal (ColumnRef "ancestor" "him")   (ColumnRef "grandparent" "him"),
-                            Equal (ColumnRef "ancestor" "me")    (SqlStr "yuki"),
-                            Equal (ColumnRef "grandparent" "me") (SqlStr "masaki") ]
-          }
-
-test_genSQLASTWithNoRecursiveWithClause :: Assertion
-test_genSQLASTWithNoRecursiveWithClause = sql @=? genSQLAST (M.fromList [("grandparent", ruleGrandParent)] ) (DatalogQuery queryHead queryBody)
-  where
-    -- grandparent(me: X, him: Y) :- parent(me: X, him: P), parent(me: P, him: Y).
-    ruleGrandParent :: DatalogStmt
-    ruleGrandParent = DatalogRule [ (qhead, body) ]
-      where
-        qhead :: DatalogHead
-        qhead = DatalogHead [
-                 TupleAttrRef {rel=Relation{name="grandparent", rid=0}, attr="me", arg=Var "X"},
-                 TupleAttrRef {rel=Relation{name="grandparent", rid=0}, attr="him", arg=Var "Y"}
-               ]
-        body :: DatalogBody
-        body = DatalogBody [
-                 TupleAttrRef {rel=Relation{name="parent", rid=0}, attr="me", arg=Var "X"},
-                 TupleAttrRef {rel=Relation{name="parent", rid=0}, attr="him", arg=Var "P"},
-                 TupleAttrRef {rel=Relation{name="parent", rid=1}, attr="me", arg=Var "P"},
-                 TupleAttrRef {rel=Relation{name="parent", rid=1}, attr="him", arg=Var "Y"}
-               ]
-
-    -- ?(me: X, him: Y) :- grandparent(me: X, him: Y).
-    queryHead :: DatalogHead
-    queryHead = DatalogHead [ 
-                  TupleAttrRef {rel=Relation{name="?", rid=0}, attr="me", arg=Var "X"},
-                  TupleAttrRef {rel=Relation{name="?", rid=0}, attr="him", arg=Var "Y"}
-                ]
-
-    queryBody :: DatalogBody
-    queryBody = DatalogBody [
-                  TupleAttrRef {rel=Relation{name="grandparent", rid=0}, attr="me", arg=Atom "X"},
-                  TupleAttrRef {rel=Relation{name="grandparent", rid=0}, attr="him", arg=Var "Y"}
-                ]
-    -- with grandparent(me, him) as (
-    --   select parent1.me as me
-    --        , parent2.him as him
-    --   from parent as parent1
-    --        parent as parent2
-    --   where true
-    --     and parent1.him = parent2.me
-    -- )
-    -- select grandparent.me as me
-    --      , grandparent.him as him
-    -- from grandparent
-    -- ;
-    sql :: SelectStmt
-    sql = SelectStmt {
-            withClauses = [grandparent],
-            selectExprs = [ SelectExpr (ColumnRef "ancestor" "me") (Just "me")
-                          , SelectExpr (ColumnRef "ancestor" "him") (Just "him") ],
-            fromTables = [ Table "ancestor" Nothing, Table "grandparent" Nothing ],
-            whereClause = [ Equal (ColumnRef "ancestor" "him")   (ColumnRef "grandparent" "him"),
-                            Equal (ColumnRef "ancestor" "me")    (SqlStr "yuki"),
-                            Equal (ColumnRef "grandparent" "me") (SqlStr "masaki") ]
-          }
-      where
-        grandparent :: CTE
-        grandparent = CTE "grandparent" SelectStmt {
-          withClauses=[],
-          selectExprs=[ SelectExpr (ColumnRef "parent1" "me") (Just "me")
-                      , SelectExpr (ColumnRef "parent2" "him") (Just "him") ],
-          fromTables=[ Table "parent" (Just "parent1"), Table "parent" (Just "parent2") ],
-          whereClause=[ Equal (ColumnRef "parent1" "him") (ColumnRef "parent2" "me") ]
-        }
 
 test_indent :: Assertion
 test_indent = expected @=? indent is
