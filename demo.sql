05023a067b8810568ccc64bd044102d008cddd5e
Yuki Takeichi <yuki.takeichi@gmail.com>
2015-10-08 19:31:46 +0900

    Demo by Isono family

---
+++ demo.sql
@@ -0,0 +1,10 @@
+drop table father;
+drop table mother;
+
+create table father ( of text, me text) ;
+create table mother ( of text, me text) ;
+
+insert into father (of, me) values('katsuo', 'masuo');
+insert into mother (of, me) values('katsuo', 'sazae');
+insert into father (of, me) values('sazae', 'fune');
+insert into mother (of, me) values('sazae', 'namihei');
