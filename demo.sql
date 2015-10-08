548afa12c8ed51783b6f1f43c102b057b369ddcb
Yuki Takeichi <yuki.takeichi@gmail.com>
2015-10-08 20:15:46 +0900

    fix

--- demo.sql
+++ demo.sql
@@ -4,7 +4,7 @@ drop table mother;
 create table father ( of text, me text) ;
 create table mother ( of text, me text) ;
 
-insert into father (of, me) values('katsuo', 'masuo');
-insert into mother (of, me) values('katsuo', 'sazae');
+insert into father (of, me) values('tara', 'masuo');
+insert into mother (of, me) values('tara', 'sazae');
 insert into father (of, me) values('sazae', 'fune');
 insert into mother (of, me) values('sazae', 'namihei');
